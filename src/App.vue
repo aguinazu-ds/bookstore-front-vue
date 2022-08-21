aa6741e5d3e6072e05f8a3128ddb8676dc01c77d
aeguinazu-ds <aeguinazu@gmail.com>
2022-08-20 23:32:06 -0400

    front

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Header />
   <router-view />
 </template>
-
+<script>
+import Header from "./components/HeaderStore.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
 <style>
+@import url("https://fonts.googleapis.com/css?family=Exo+2");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  background-color: #f8f8f8 !important;
+  font-family: "Exo 2", sans-serif !important;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
